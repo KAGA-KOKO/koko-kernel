23fbdf4499263a5efeaa4e9b613b024e3c25a2b3
KAGA-KOKO <kotakandis185@gmail.com>
2024-11-13 17:37:46 +0000

    oppo_fp_common: fix build error

--- drivers/input/oppo_fp_drivers/oppo_fp_common/oppo_fp_common.c
+++ drivers/input/oppo_fp_drivers/oppo_fp_common/oppo_fp_common.c
@@ -128,7 +128,7 @@ fp_module_config_t fp_module_config_list[] = {
 fp_module_config_t fp_module_config_list[] = {
     {{1, -1, -1},  FP_UNKNOWN,    	CHIP_UNKNOWN,     ENGINEER_MENU_FPC1023},
     {{1, -1, -1},  FP_UNKNOWN,    	CHIP_UNKNOWN,     ENGINEER_MENU_FPC1023},
-
+};
 #endif
 
 static int fp_request_named_gpio(struct fp_data *fp_data,
@@ -481,4 +481,4 @@ static void __exit oppo_fp_common_exit(void)
 }
 
 subsys_initcall(oppo_fp_common_init);
-module_exit(oppo_fp_common_exit)
+module_exit(oppo_fp_common_exit);
